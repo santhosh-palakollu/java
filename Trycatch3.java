f9dfd7b657e7f99c92b05f5a1b72e23674fc681d
santhosh-palakollu <151706109+santhosh-palakollu@users.noreply.github.com>
2024-10-15 14:28:05 +0530

    Trycatch3.java

---
+++ Trycatch3.java
@@ -0,0 +1,24 @@
+import java.io.*;
+class Trycatch3
+{
+	public static void main(String args[])
+	{
+		int a=5,b=0,c;
+		try
+		{
+			c=a/b;
+			System.out.println("Value of c is "+c);
+		}
+		
+		catch(ArithmeticException ae)
+		{
+			System.out.println("ArithmeticException caught "+ae);
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("Exception caught "+e);
+		}
+		
+	}
+}
