c840ea624701effce6900a8458bc3b29974799ba
santhosh-palakollu <151706109+santhosh-palakollu@users.noreply.github.com>
2024-10-19 11:08:04 +0530

    interfaceDemo.java

---
+++ interfaceDemo.java
@@ -0,0 +1,28 @@
+interface Drawable
+{  
+	void draw();  
+}  
+ 
+class Rectangle implements Drawable
+{  
+	public void draw()
+	{
+		System.out.println("drawing rectangle");
+	}  
+}  
+class Circle implements Drawable
+{  
+	public void draw()
+	{
+		System.out.println("drawing circle");
+	}  
+}  
+ 
+class InterfaceDemo
+{  
+	public static void main(String args[])
+	{  
+		Drawable d=new Circle();   
+		d.draw();  
+	}
+}  
