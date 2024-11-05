9e686e18b4108631af5f9557782cac4c8fdca367
santhosh-palakollu <151706109+santhosh-palakollu@users.noreply.github.com>
2024-11-05 14:13:24 +0530

    StringDemo.java

---
+++ StringDemo.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.*;
+public class StringDemo
+{
+	public static void main(String args[])
+	{	String s1="welcome";
+		char ch[]={'S','A','N','T','H','O','S','H'};
+		String s2=new String(ch);
+		String s3=new String("example");
+		System.out.println("String S1 is "+s1);
+		System.out.println("String S2 is "+s2);
+		System.out.println("String S3 is "+s3);
+	}
+}
